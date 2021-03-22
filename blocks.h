aa1a959c86d755881d958cec0bfaa98d0887d14a
WouterSpekkink <wahsp@tutanota.com>
2021-03-22 23:37:32 +0100

    customized my blocks.

---
+++ blocks.h
@@ -0,0 +1,11 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+/*Icon*/ /*Command*/ /*Update Interval*/ /*Update Signal*/
+        {"", "sb-battery", 5, 3},
+        {"", "sb-cpu", 10, 18},
+        {"", "sb_memory", 10, 14},
+        {"", "sb-clock", 60, 1},
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char *delim = " | ";
