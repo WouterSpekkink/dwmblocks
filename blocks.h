0b858c5b1b151458ab304b643bd9b578314af2fc
WouterSpekkink <wahsp@tutanota.com>
2021-04-02 17:49:03 +0200

    Removed cpu.

--- blocks.h
+++ blocks.h
@@ -1,10 +1,12 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 /*Icon*/ /*Command*/ /*Update Interval*/ /*Update Signal*/
-        {"", "sb-battery", 5, 3},
-        {"", "sb-cpu", 10, 18},
-        {"", "sb-memory", 10, 14},
-        {"", "sb-clock", 60, 1},
+        {"", "sb-volume", 0, 1},
+        {"", "sb-battery", 5, 2},
+ //       {"", "sb-cpu", 10, 3},
+        {"", "sb-memory", 10, 4},
+        {"", "sb-clock", 60, 5},
+        {"", "sb-internet", 5, 6},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
